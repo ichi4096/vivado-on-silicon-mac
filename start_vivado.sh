bd2b8b92c6d5860b9e721adb2266b7955f6c785d
Michael Mösch <mmoesch@student.ethz.ch>
2023-04-16 17:58:48 +0200

    Add settings64.sh to start_vivado.sh

--- start_vivado.sh
+++ start_vivado.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /home/user
 /home/user/Xilinx/Vivado/*/bin/hw_server -e "set auto-open-servers     xilinx-xvc:host.docker.internal:2542" &
+/home/user/Xilinx/Vivado/*/settings64.sh
 /home/user/Xilinx/Vivado/*/bin/vivado

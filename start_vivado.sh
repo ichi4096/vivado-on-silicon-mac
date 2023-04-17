da45ccd1c30b92e18ee2cf31660e7082f256baa2
Michael Mösch <mmoesch@student.ethz.ch>
2023-04-18 00:24:39 +0200

    Add libz to start_vivado.sh

--- start_vivado.sh
+++ start_vivado.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/user
-export LD_PRELOAD="/lib/x86_64-linux-gnu/libudev.so.1 /lib/x86_64-linux-gnu/libselinux.so.1 /lib/x86_64-linux-gnu/libgdk-x11-2.0.so.0"
+export LD_PRELOAD="/lib/x86_64-linux-gnu/libudev.so.1 /lib/x86_64-linux-gnu/libselinux.so.1 /lib/x86_64-linux-gnu/libz.so.1 /lib/x86_64-linux-gnu/libgdk-x11-2.0.so.0"
 export JAVA_TOOL_OPTIONS="-Dsun.java2d.xrender=false"
 export JAVA_OPTS="-Dsun.java2d.xrender=false"
 export DISPLAY="host.docker.internal:0"

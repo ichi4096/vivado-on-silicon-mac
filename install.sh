b525e7828e1253080e5f8f4f8400960e253904b5
Michael Mösch <mmoesch@student.ethz.ch>
2023-04-17 14:07:37 +0200

    Enable GLX
    and add space so the command doesn't appear in terminal history

--- install.sh
+++ install.sh
@@ -32,6 +32,9 @@ fi
 defaults write org.xquartz.X11 no_auth 1
 defaults write org.xquartz.X11 nolisten_tcp 0
 
+# Vivado seems to be using GLX
+defaults write org.xquartz.X11 enable_iglx -bool true
+
 # Launch Docker daemon and XQuartz
 f_echo "Launching Docker daemon and XQuartz..."
 open -a XQuartz
@@ -83,5 +86,5 @@ echo "#!/bin/zsh\nopen -a XQuartz\nopen -a Docker\nwhile ! /usr/local/bin/docker
 # Run docker container by starting hw_server first to establish an XVC connection and then Vivado
 echo "/usr/local/bin/docker run --rm --name vivado_container --mount type=bind,source=\"/tmp/.X11-unix\",target=\"/tmp/.X11-unix\" --mount type=bind,source=\""$script_dir"\",target=\"/home/user\" --platform linux/amd64 x64-linux sudo -H -u user bash /home/user/start_vivado.sh &" >> Launch_Vivado.app/Launch_Vivado
 # Launch XVC server on host
-echo "osascript -e 'tell app \"Terminal\" to do script \"while ! [[ \$(ps aux | grep vivado_container | wc -l | tr -d \\\"\\\\\\\n\\\\\\\t \\\") == \\\"1\\\" ]]; do "$script_dir"/xvcd/bin/xvcd; sleep 1; done; exit\"'" >> Launch_Vivado.app/Launch_Vivado
+echo "osascript -e 'tell app \"Terminal\" to do script \" while ! [[ \$(ps aux | grep vivado_container | wc -l | tr -d \\\"\\\\\\\n\\\\\\\t \\\") == \\\"1\\\" ]]; do "$script_dir"/xvcd/bin/xvcd; sleep 1; done; exit\"'" >> Launch_Vivado.app/Launch_Vivado
 chmod +x Launch_Vivado.app/Launch_Vivado
